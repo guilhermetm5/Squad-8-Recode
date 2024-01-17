bc5955b53d374775c7cfd136f73f1842c7ed74eb
Joao Guilherme <joaosilvabarbosa001@gmail.com>
2024-01-17 09:10:04 -0300

    ajustes de login

---
+++ javascript/scripts.js
@@ -0,0 +1,11 @@
+// Criando lógica do form de login
+const email = 'teste@email';
+const senha = 1234;
+
+
+function obterValor(ver1 = email) {
+    var ver1 = document.getElementById('teste')
+    alert("Usuário Logado!")
+    if (alert = "Usuário Logado!") {
+    }
+}
